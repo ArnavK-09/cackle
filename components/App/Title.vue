d165ae3c3933dc5519c8adbd246c5951c7dba1ba
Arnav K <arnavkaushal09@gmail.com>
2023-01-17 23:14:58 +0530

    Removed shadow from title

--- components/App/Title.vue
+++ components/App/Title.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="sticky top-0 px-4 drop-shadow-md py-3 bg-white/80 backdrop-blur-sm dark:bg-transparent">
+    <div class="sticky top-0 px-4 py-3 bg-white/80 backdrop-blur-sm dark:bg-transparent">
         <h2 class="capitalize  text-xl font-bold text-gray-800 dark:text-gray-100">{{ title }}</h2>
     </div>
 </template>
